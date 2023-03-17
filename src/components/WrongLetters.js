5cccabf6914d6b327658628a38867a590ed7fbcf
Lily Roh <[eunhyeroh94@gmail.com]>
2023-03-17 14:36:38 +0000

    changed help alret

--- src/components/WrongLetters.js
+++ src/components/WrongLetters.js
@@ -4,7 +4,18 @@ import React from 'react'
 const WrongLetters = ({ wrongLetters }) => {
   // Define a function to be called when the help button is clicked
   const help = (e) =>{
-  alert("Guess the word and save this man!!");
+  alert(
+    "1) The game randomly selects a word from a pre-defined list of words." +
+
+  "\n2) You are given 6 chances to guess the word by guessing one letter at a time."+
+  
+  "\n3) For each incorrect guess, a part of the hangman is drawn on the screen, representing a person being hanged. The number of incorrect guesses allowed before the 'hangman' is complete and the game is lost is usually predetermined."
+  
+  +"\n4) If  you guesses a letter that appears in the word, all instances of that letter in the word are revealed." +
+  
+  "\n5) You wins the game if you correctly guess the entire word before the hangman is fully drawn."
+  
+  +"\n6) If you loses the game the correct word is revealed and the game is over");
 }
   return (
     <div className="wrong-letters-container">
